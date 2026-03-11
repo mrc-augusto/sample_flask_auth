ec96f3740502badb59f7d47127170922122fc481
Maurício Augusto <mrc_augusto@hotmail.com>
2026-03-11 13:34:28 -0300

    feat: Add logout functionality to user authentication

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -34,7 +34,13 @@ def login():
     
   return jsonify({'message': 'Credenciais inválidas'}), 400
 
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({'message': 'Logout bem sucedido'})
 
+  
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
